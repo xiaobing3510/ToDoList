9fb8bdb2376b5b7f402c759ad72286a386605130
chenjie <xiaobing3510@gmail.com>
2022-10-29 19:49:23 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <section class="todoapp">
+    <!-- 除了驼峰, 还可以使用-转换链接 -->
+    <TodoHeader></TodoHeader>
+    <TodoMain></TodoMain>
+    <TodoFooter></TodoFooter>
+  </section>
+</template>
+
+<script>
+// 1.0 样式引入
+import "./styles/base.css"
+import "./styles/index.css"
+    
+import TodoHeader from "./components/TodoHeader";
+import TodoMain from "./components/TodoMain";
+import TodoFooter from "./components/TodoFooter";
+
+
+export default {
+  components: {
+    TodoHeader,
+    TodoMain,
+    TodoFooter,
+  },
+  data() {
+    return {
+      list: [
+        { id: 100, name: "吃饭", isDone: true },
+        { id: 101, name: "睡觉", isDone: false },
+        { id: 102, name: "打豆豆", isDone: true },
+      ],
+    };
+  }
+};
+</script>
---
+++ src/main.js
@@ -0,0 +1,11 @@
+import Vue from 'vue'
+import App from './App.vue'
+
+import "./styles/base.css"
+import "./styles/index.css"
+
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
